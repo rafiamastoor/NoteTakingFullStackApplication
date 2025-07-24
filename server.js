5d52b6580b8e6de3be99fa832fb27cbab4b6e274
rafiamastoor <rafiamastoor@gmail.com>
2025-07-24 17:06:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const port = 3000;
+const port = 80;
 const app = express();
 
 app.use(express.json());
